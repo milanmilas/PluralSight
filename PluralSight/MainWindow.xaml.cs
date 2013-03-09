5d98ddebb369ab58271207debee399c6a8be1bd9
Milan Milas <milanmilas@gmail.com>
2013-03-09 16:43:51 +0000

    mmm

--- PluralSight/MainWindow.xaml.cs
+++ PluralSight/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using PluralSightProcessor;
+using PluralSightProcessor; 
 using PluralSightProcessor.Domain;
 using PluralSight.ViewModel;
 
